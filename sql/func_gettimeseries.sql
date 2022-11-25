a4d14e7aca5fc417a68e54ea563b8a891715e9e1
hendriksen <gerrit.hendriksen@deltares.nl>
2022-11-25 10:42:51 +0100

    extracomponents added to output of the query

--- sql/func_gettimeseries.sql
+++ sql/func_gettimeseries.sql
@@ -1,5 +1,5 @@
--- create or replace function timeseries.gwsfiltertimeseries (loc_id,parameter) returns setof json as 
--- $$
+create or replace function timeseries.gwsfiltertimeseries (loc_id text,parameter text) returns setof json 
+as $$
 SELECT json_build_object(
     'locationproperties',json_build_object(
 		'locationid',l.name,
@@ -9,6 +9,11 @@ SELECT json_build_object(
 		'top_filter',l.tubetop,
 		'bot_filter',l.tubebot,
 		'cable_length',l.cablelength),
+	'locationstats',json_build_object(
+		'mingw',0.5,
+		'maxgw',0.8,
+		'meangw',0.75,
+		'nobs',19000),
 	'parameterproperties',json_build_object(
 		'parameter',p.name,
 		'unit',u.unit),
@@ -17,7 +22,7 @@ SELECT json_build_object(
                 -- list of fields
                 'datetime', tsv.datetime,
                 'head',tsv.scalarvalue,
-			    'correctedhead',tsv.scalarvalue*13.25/0.0053
+			    'correctedhead',tsv.scalarvalue*0.93
             )
          )
 ) 
@@ -26,5 +31,7 @@ join timeseries.timeseries sk on sk.locationkey=l.locationkey
 join timeseries.parameter p on p.parameterkey = sk.parameterkey
 join timeseries.unit u on u.unitkey = p.unitkey
 join timeseries.timeseriesvaluesandflags tsv on tsv.timeserieskey = sk.timeserieskey
-where l.name = 'A_2' and p.description = 'Divermeting: grondwaterstand'
+where l.name = loc_id and p.description = parameter
 group by l.name, p.name,u.unit,x,y,tubetop,tubebot,cablelength,epsgcode
+$$ 
+language sql
