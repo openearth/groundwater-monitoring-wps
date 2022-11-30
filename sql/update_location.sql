8f5e3b6e6ac945ff43a9350707a1c538ca93b9d3
nathaliedees <nathaliedees@hotmail.com>
2022-11-30 13:09:56 +0100

    Create update_location.sql
    updating the location table with max_gw, min_gw, nobs and corr_factor

---
+++ sql/update_location.sql
@@ -0,0 +1,50 @@
+--ec, taken from last september
+with e as (select tm.scalarvalue as ec,l.name as locid from timeseries.timeseries ts
+join timeseries.parameter pa ON pa.parameterkey = ts.parameterkey
+join timeseries.unit u on pa.unitkey = u.unitkey
+join timeseries.location l on ts.locationkey= l.locationkey
+join timeseries.timeseriesvaluesandflags tm on ts.timeserieskey = tm.timeserieskey
+where pa.parameterkey=4 and tm.datetime = '2022-08-09')
+update timeseries.location l
+set corr_factor=e.ec
+from e
+where l.name = e.locid
+
+--min_gw
+with e as (select min(tm.scalarvalue) as min_gws,l.name as locid from timeseries.timeseries ts
+join timeseries.parameter pa ON pa.parameterkey = ts.parameterkey
+join timeseries.unit u on pa.unitkey = u.unitkey
+join timeseries.location l on ts.locationkey= l.locationkey
+join timeseries.timeseriesvaluesandflags tm on ts.timeserieskey = tm.timeserieskey
+where pa.parameterkey=9 
+group by locid)
+update timeseries.location l
+set min_gw=e.min_gws
+from e
+where l.name = e.locid
+
+--max_gw
+with e as (select max(tm.scalarvalue) as max_gws,l.name as locid from timeseries.timeseries ts
+join timeseries.parameter pa ON pa.parameterkey = ts.parameterkey
+join timeseries.unit u on pa.unitkey = u.unitkey
+join timeseries.location l on ts.locationkey= l.locationkey
+join timeseries.timeseriesvaluesandflags tm on ts.timeserieskey = tm.timeserieskey
+where pa.parameterkey=9 
+group by locid)
+update timeseries.location l
+set max_gw=e.max_gws
+from e
+where l.name = e.locid
+
+--nobs
+with e as (select count(tm.scalarvalue) as nobs,l.name as locid from timeseries.timeseries ts
+join timeseries.parameter pa ON pa.parameterkey = ts.parameterkey
+join timeseries.unit u on pa.unitkey = u.unitkey
+join timeseries.location l on ts.locationkey= l.locationkey
+join timeseries.timeseriesvaluesandflags tm on ts.timeserieskey = tm.timeserieskey
+where pa.parameterkey=8
+group by locid)
+update timeseries.location l
+set nobs=e.nobs
+from e
+where l.name = e.locid
