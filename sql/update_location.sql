b586d9505e930a26e1919b8db24588e0ef8c12cf
hendriksen <gerrit.hendriksen@deltares.nl>
2022-11-30 13:31:03 +0100

    added routine for mean_head

--- sql/update_location.sql
+++ sql/update_location.sql
@@ -48,3 +48,16 @@ update timeseries.location l
 set nobs=e.nobs
 from e
 where l.name = e.locid
+
+--mean_head
+with e as (select avg(tm.scalarvalue) as mean_head,l.name as locid from timeseries.timeseries ts
+join timeseries.parameter pa ON pa.parameterkey = ts.parameterkey
+join timeseries.unit u on pa.unitkey = u.unitkey
+join timeseries.location l on ts.locationkey= l.locationkey
+join timeseries.timeseriesvaluesandflags tm on ts.timeserieskey = tm.timeserieskey
+where pa.parameterkey=9 
+group by locid)
+update timeseries.location l
+set mean_head=e.mean_head
+from e
+where l.name = e.locid
